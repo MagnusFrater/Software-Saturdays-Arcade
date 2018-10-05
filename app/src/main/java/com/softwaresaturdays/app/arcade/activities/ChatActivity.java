83334efa612108bae39270ff2e740d0906653d03
Nishant Jain <jain131@purdue.edu>
2018-10-05 11:05:22 -0400

    Add google sign in

---
+++ app/src/main/java/com/softwaresaturdays/app/arcade/activities/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.softwaresaturdays.app.arcade.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softwaresaturdays.app.arcade.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
--- app/src/main/java/com/softwaresaturdays/app/arcade/activities/LoginActivity.java
+++ app/src/main/java/com/softwaresaturdays/app/arcade/activities/LoginActivity.java
@@ -1,15 +1,94 @@
 package com.softwaresaturdays.app.arcade.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.softwaresaturdays.app.arcade.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN = 2;
+    public static final String TAG = "LOGIN_ACTIVITY";
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton mSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("456216257726-rls29efgrvnd3hon2d8f7jpbcvjgsbut.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mSignIn = findViewById(R.id.sign_in_button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account == null) {
+            mSignIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                    startActivityForResult(signInIntent, RC_SIGN_IN);
+                }
+            });
+        } else {
+            // User already signed in, go to chat activity
+            startActivity(new Intent(getApplicationContext(), ChatActivity.class));
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            // Signed in successfully
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // TODO Got user info, Create user object and update on Database
+            Log.w(TAG, "SUCCESSFUL SIGN IN EMAIL: " + account.getEmail());
+
+
+            // User signed in, go to chat activity
+            startActivity(new Intent(getApplicationContext(), ChatActivity.class));
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
     }
 }
