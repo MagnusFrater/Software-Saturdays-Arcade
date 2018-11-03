27663b2094a64dfa255a2e1fa12887a3f5bcb774
Todd Griffin <nemagnusfrater@gmail.com>
2018-11-03 01:18:12 -0400

    Complete offline, non-AI Tic Tac Toe gameplay.

--- app/src/main/java/com/softwaresaturdays/app/arcade/activities/games/TicTacToe.java
+++ app/src/main/java/com/softwaresaturdays/app/arcade/activities/games/TicTacToe.java
@@ -14,16 +14,22 @@ import com.softwaresaturdays.app.arcade.utilities.Util;
 
 public class TicTacToe extends AppCompatActivity {
 
-    private GridLayout glBoard;
+    private enum STATE {
+        YOU, OPPONENT, GAME_OVER
+    }
 
-    final private int[][] magicSquare = {
+    private final String[] symbols = {"X", "O"};
+    private final int[][] magicSquare = {
             {8, 1, 6},
             {3, 5, 7},
             {4, 9, 2}
     };
 
+    private GridLayout glBoard;
+
     private int[][] board;
-    private int turn = 0;
+    private STATE state;
+    private int yourSymbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,9 @@ public class TicTacToe extends AppCompatActivity {
     private void resetGame() {
         board = new int[3][3];
         populateBoardView();
+
+        state = STATE.YOU;
+        yourSymbol = 0;
     }
 
     private void populateBoardView() {
@@ -67,8 +76,11 @@ public class TicTacToe extends AppCompatActivity {
             tvCell.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(final View v) {
-                    final TextView tvCell = (TextView) v;
+                    if (state == STATE.GAME_OVER) {
+                        return;
+                    }
 
+                    final TextView tvCell = (TextView) v;
                     if (!tvCell.getText().equals("")) {
                         return;
                     }
@@ -82,10 +94,77 @@ public class TicTacToe extends AppCompatActivity {
     }
 
     private void takeTurn(final TextView tvCell) {
-        tvCell.setText((turn % 2 == 0)? "X" : "O");
+        tvCell.setText((state == STATE.YOU)? symbols[yourSymbol] : symbols[(yourSymbol + 1) % 2]);
 
-        ((TextView) findViewById(R.id.tvTurn)).setText((turn % 2 == 0)? R.string.your_turn : R.string.opponents_turn);
+        // update game board
+        final int cellTag = (int) tvCell.getTag();
+        final int y = cellTag / board.length;
+        final int x = cellTag % board.length;
+        board[y][x] = (state == STATE.YOU)? 1 : -1;
 
-        turn = ++turn % 2;
+        // check if game over
+        checkGameOver(y, x);
+
+        // switch turn if game not over
+        if (state != STATE.GAME_OVER) {
+            state = (state == STATE.YOU)? STATE.OPPONENT : STATE.YOU;
+            ((TextView) findViewById(R.id.tvTurn)).setText((state == STATE.YOU)? R.string.your_turn : R.string.opponents_turn);
+        }
+    }
+
+    private void checkGameOver(final int y, final int x) {
+        int sum;
+
+        // check column
+        sum = 0;
+        for (int i=0; i<board.length; i++) {
+            sum += board[i][x] * magicSquare[i][x];
+        }
+        if (Math.abs(sum) == 15) gameOver(false);
+
+        // check row
+        sum = 0;
+        for (int i=0; i<board.length; i++) {
+            sum += board[y][i] * magicSquare[y][i];
+        }
+        if (Math.abs(sum) == 15) gameOver(false);
+
+        // check diagonal
+        if (y == x) {
+            sum = 0;
+            for (int i=0; i<board.length; i++) {
+                sum += board[i][i] * magicSquare[i][i];
+            }
+            if (Math.abs(sum) == 15) gameOver(false);
+        }
+
+        // check anti-diagonal
+        if (y + x == board.length - 1) {
+            sum = 0;
+            for (int i=0; i<board.length; i++) {
+                sum += board[i][board.length - 1 - i] * magicSquare[i][board.length - 1 - i];
+            }
+            if (Math.abs(sum) == 15) gameOver(false);
+        }
+
+        // check tie
+        for (int yi=0; yi<board.length; yi++) {
+            for (int xi=0; xi<board.length; xi++) {
+                if (board[yi][xi] == 0) {
+                    return;
+                }
+            }
+        }
+        gameOver(true);
+    }
+
+    private void gameOver(final boolean tie) {
+        state = STATE.GAME_OVER;
+
+        if (tie) {
+            ((TextView) findViewById(R.id.tvTurn)).setText(R.string.tie);
+        } else {
+            ((TextView) findViewById(R.id.tvTurn)).setText((state == STATE.YOU)? R.string.win : R.string.lose);
+        }
     }
 }
