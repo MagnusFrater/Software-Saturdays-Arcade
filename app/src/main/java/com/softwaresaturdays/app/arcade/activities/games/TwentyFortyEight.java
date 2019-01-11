c04b5f2d185717a5feaafa12dfd3c337d2bce39d
Todd Everett Griffin <goddtriffin@gmail.com>
2019-01-10 20:22:17 -0500

    Comment the 2048 game activity.

--- app/src/main/java/com/softwaresaturdays/app/arcade/activities/games/TwentyFortyEight.java
+++ app/src/main/java/com/softwaresaturdays/app/arcade/activities/games/TwentyFortyEight.java
@@ -25,11 +25,11 @@ import java.util.List;
 
 public class TwentyFortyEight extends GameActivity implements View.OnTouchListener {
 
-    private GridLayout glBoard;
-    private GestureDetector gestureDetector;
+    private GridLayout glBoard;  // the game board where the we will generate the tiles
+    private GestureDetector gestureDetector;  // helps us attach finger swiping motions to functions
 
-    private int[][] board;
-    private int mScore;
+    private int[][] board;  // holds the board/game state
+    private int mScore;  // player's current score
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -37,9 +37,12 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twenty_fourty_eight);
 
+        // tells the Activity that we want to listen for finger touch events
         findViewById(R.id.clLayout).setOnTouchListener(this);
 
         glBoard = findViewById(R.id.glBoard);
+
+        // connect finger gestures to their relative functions
         gestureDetector = new GestureDetector(this, new OnSwipeListener() {
 
             @Override
@@ -70,6 +73,7 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
             }
         });
 
+        // reset for a new game
         resetGame();
     }
 
@@ -80,20 +84,25 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
         return true;
     }
 
+    // make it a new game
     private void resetGame() {
         board = new int[4][4];
-        birthCell();
+        birthTile();
         updateScore(0);
         populateBoardView();
     }
 
+    // dynamically translate the board state to the actual on-screen board
     private void populateBoardView() {
+        // clear the board of all entities
         glBoard.removeAllViews();
 
+        // for every row/column,
         for (int y=0; y<board.length; y++) {
             for (int x=0; x<board.length; x++) {
-                final TextView tvCell = new TextView(this);
-                tvCell.setBackgroundColor(getCellBackgroundColor(board[y][x])); // hex color 0xAARRGGBB
+                // create a new tile and set all of its stats
+                final TextView tvTile = new TextView(this);
+                tvTile.setBackgroundColor(getTileBackgroundColor(board[y][x])); // hex color 0xAARRGGBB
 
                 final int size = (int) Util.convertDpToPixel(80, this);
                 final int margin = (int) Util.convertDpToPixel(8, this);
@@ -104,21 +113,23 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
                         margin,
                         margin
                 );
-                tvCell.setLayoutParams(params);
+                tvTile.setLayoutParams(params);
 
-                tvCell.setTextSize(35);
-                tvCell.setGravity(Gravity.CENTER);
-                tvCell.setTypeface(null, Typeface.BOLD);
-                tvCell.setTextColor(getCellTextColor(board[y][x]));
-                tvCell.setText(((board[y][x] > 0) ? String.valueOf(board[y][x]) : ""));
+                tvTile.setTextSize(35);
+                tvTile.setGravity(Gravity.CENTER);
+                tvTile.setTypeface(null, Typeface.BOLD);
+                tvTile.setTextColor(getTileTextColor(board[y][x]));
+                tvTile.setText(((board[y][x] > 0) ? String.valueOf(board[y][x]) : ""));  // set the new tile's value to a power of two, or keep it blank
 
-                glBoard.addView(tvCell);
+                // push it to the fresh game board
+                glBoard.addView(tvTile);
             }
         }
     }
 
-    private int getCellBackgroundColor(final int cellValue) {
-        switch (cellValue) {
+    // gives the correct tile color based on the tile's value
+    private int getTileBackgroundColor(final int tileValue) {
+        switch (tileValue) {
             case 2:
                 return 0xffece2d8;
             case 4:
@@ -148,8 +159,9 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
         }
     }
 
-    private int getCellTextColor(final int cellValue) {
-        switch (cellValue) {
+    // returns the correct tile text color based on the tile's value
+    private int getTileTextColor(final int tileValue) {
+        switch (tileValue) {
             case 2:
             case 4:
                 return 0xff776e65;
@@ -160,7 +172,7 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
 
     private void swipeUp() {
         if (moveUp()) {
-            birthCell();
+            birthTile();
             checkGameOver();
         }
         populateBoardView();
@@ -168,7 +180,7 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
 
     private void swipeDown() {
         if (moveDown()) {
-            birthCell();
+            birthTile();
             checkGameOver();
         }
         populateBoardView();
@@ -176,7 +188,7 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
 
     private void swipeLeft() {
         if (moveLeft()) {
-            birthCell();
+            birthTile();
             checkGameOver();
         }
         populateBoardView();
@@ -184,28 +196,36 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
 
     private void swipeRight() {
         if (moveRight()) {
-            birthCell();
+            birthTile();
             checkGameOver();
         }
         populateBoardView();
     }
 
+    // moves a tile to a different location on the game board
+    // return true if move was made, false otherwise
     public boolean moveTo(int fromRow, int fromCol, int toRow, int toCol) {
+        // always check if coordinates are within the bounds of the game board
         if (isCoordinateOutOfBounds(fromRow, fromCol) || isCoordinateOutOfBounds(toRow, toCol)) {
             return false;
         }
 
+        // grab the values of the tile to move and the tile to move to
         final int from = board[fromRow][fromCol];
         final int to = board[toRow][toCol];
 
+        // if the current tile doesn't exist, don't move it
         if (from == 0) {
             return false;
         }
 
+        // move the tile only if the goal tile is empty or is the same value as the tile to move
         if (to == 0 || to == from) {
+            // move the tile
             board[toRow][toCol] = from + to;
             board[fromRow][fromCol] = 0;
 
+            // upon a tile merge, increase the score
             if (to == from) {
                 updateScore(board[toRow][toCol]);
             }
@@ -216,6 +236,7 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
         return false;
     }
 
+    // move all tiles upwards as far as they can go
     public boolean moveUp() {
         int cycles = 0;
         boolean madeMove = true;
@@ -234,6 +255,7 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
         return cycles > 1;
     }
 
+    // move all tiles downwards as far as they can go
     public boolean moveDown() {
         int cycles = 0;
         boolean madeMove = true;
@@ -252,24 +274,7 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
         return cycles > 1;
     }
 
-    public boolean moveRight() {
-        int cycles = 0;
-        boolean madeMove = true;
-        while (madeMove) {
-            madeMove = false;
-
-            for (int x = 0; x < board.length; x++) {
-                for (int y = board.length - 1; y >= 0; y--) {
-                    madeMove = moveTo(x, y, x, y + 1) || madeMove;
-                }
-            }
-
-            cycles++;
-        }
-
-        return cycles > 1;
-    }
-
+    // move all tiles leftwards as far as they can go
     public boolean moveLeft() {
         int cycles = 0;
         boolean madeMove = true;
@@ -288,48 +293,74 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
         return cycles > 1;
     }
 
-    private List<Integer> getUnusedCells() {
-        final List<Integer> unusedCells = new ArrayList<>();
+    // move all tiles rightwards as far as they can go
+    public boolean moveRight() {
+        int cycles = 0;
+        boolean madeMove = true;
+        while (madeMove) {
+            madeMove = false;
+
+            for (int x = 0; x < board.length; x++) {
+                for (int y = board.length - 1; y >= 0; y--) {
+                    madeMove = moveTo(x, y, x, y + 1) || madeMove;
+                }
+            }
+
+            cycles++;
+        }
+
+        return cycles > 1;
+    }
+
+    // returns a list of tiles that are blank
+    // (x, y) translated to y * `board length` + x
+    // ie. (0, 0) => 0 , (1, 0) => 1 , (0, 1) => 3 , (1, 1) => 4 , ... , (2, 2) => 8
+    private List<Integer> getUnusedTiles() {
+        final List<Integer> unusedTiles = new ArrayList<>();
         for (int y=0; y<board.length; y++) {
             for (int x=0; x<board.length; x++) {
                 if (board[y][x] == 0) {
-                    unusedCells.add(y*board.length + x);
+                    unusedTiles.add(y*board.length + x);
                 }
             }
         }
 
-        return unusedCells;
+        return unusedTiles;
     }
 
-    private void birthCell() {
-        final List<Integer> unusedCells = getUnusedCells();
+    // randomly fill a single blank tile with either a 2 or a 4
+    private void birthTile() {
+        final List<Integer> unusedTiles = getUnusedTiles();
 
-        // make sure there are actually some unused cells
-        if (unusedCells.size() == 0) return;
+        // make sure there are actually some unused tiles
+        if (unusedTiles.size() == 0) return;
 
-        // pick random unused cell to birth
-        final int unusedCell = unusedCells.get(Util.getRandInt(0, unusedCells.size()-1));
-        final int y = unusedCell / board.length;
-        final int x = unusedCell % board.length;
+        // pick random unused tile to birth
+        final int unusedTile = unusedTiles.get(Util.getRandInt(0, unusedTiles.size()-1));
+        final int y = unusedTile / board.length;
+        final int x = unusedTile % board.length;
         board[y][x] = (Util.getRandInt(0,3) == 0)? 4 : 2;
     }
 
+    // return true if coordinate is within the bounds of the game board, false otherwise
     private boolean isCoordinateOutOfBounds(final int y, final int x) {
         return y < 0 || y > board.length - 1 || x < 0 || x > board.length - 1;
     }
 
-    private void updateScore(final int update) {
-        if (update == 0) {
+    // add new points to total, however reset it if the new points are equal to 0
+    private void updateScore(final int points) {
+        if (points == 0) {
             mScore = 0;
         } else {
-            mScore += update;
+            mScore += points;
         }
 
         ((TextView) findViewById(R.id.tvScore)).setText(String.valueOf(mScore));
     }
 
+    // if there are no more blank tiles, and if no tile is surrounded by a similar value, game is over
     private void checkGameOver() {
-        if (getUnusedCells().size() > 0) {
+        if (getUnusedTiles().size() > 0) {
             return;
         }
 
@@ -365,6 +396,7 @@ public class TwentyFortyEight extends GameActivity implements View.OnTouchListen
         return false;
     }
 
+    // create the popup dialog to allow the player to restart the game or go back to the chat
     private void gameOver() {
         // Record the score - Calling superclass method
         recordScore(mScore, "2048");
